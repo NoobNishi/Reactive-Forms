c02401285c323a3e00927ee2081913ebec7d5931
Nishvanth <nishvanth1206@gmail.com>
2023-04-15 13:20:28 +0530

    new-feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeatureFormComponent } from './feature-form/feature-form.component';
+import { NewFeatureComponent } from './new-feature/new-feature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeatureFormComponent
+    FeatureFormComponent,
+    NewFeatureComponent
   ],
   imports: [
     BrowserModule,
